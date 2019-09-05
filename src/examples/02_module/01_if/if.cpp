94c15e746918fad818c7e160a7f5d8751d596d2f
sakthibaur <sakthi.baur@g.austincc.edu>
2019-09-04 19:24:20 -0500

    Example for if statements and classwork 2

--- src/examples/02_module/01_if/if.cpp
+++ src/examples/02_module/01_if/if.cpp
@@ -1,9 +1,15 @@
-//write include statement for if header
-
+//write include statement (preprocessor directive) for if header
+#include "if.h"
 
 
 //write code for the is_even function that returns true if num is even, false otherwise
-
+bool is_even(int num)
+{
+	if (num % 2 == 0)
+		return true;
+	else
+		return false;
+}
 
 //write code for function named overtime that accepts a double hours and returns true if 
 //hours over 40, false otherwise
