91b40bb3c8afe0c7129d628a29e63626daad5d84
sakthibaur <sakthi.baur@g.austincc.edu>
2019-09-09 19:08:03 -0500

    If else and switch examples

--- src/examples/02_module/02_if_else/if_else.h
+++ src/examples/02_module/02_if_else/if_else.h
@@ -1,8 +1,13 @@
 //write include statement
+#include<string>
 
-
+using std::string; //this works or can do in one line of code (below, line 7)
 //write prototype for function named get_generation that accepts an int and
 //returns a string
+//std::string get_generation(int year); //probably better to use this way if only uisng a couple of times through code
+string get_generation(int year);
+
+
 
 
 
--- src/examples/02_module/03_switch/switch.h
+++ src/examples/02_module/03_switch/switch.h
@@ -1,8 +1,9 @@
 //write string include
 #include<string>
 
+using std::string; //this way is more functional than std::string menu below
 //Write prototype for function that accepts a num and returns a string
-
+string menu(int menu_option);
 
 
 
