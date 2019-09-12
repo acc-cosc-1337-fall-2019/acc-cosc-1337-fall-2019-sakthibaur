4421b2e876c10e0e02f552a845685b407f14306d
Sakthi Baur <sakthi.baur@g.austincc.edu>
2019-09-12 08:22:56 -0500

    Homework 2 Completed

--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -6,7 +6,18 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
-TEST_CASE("Verify get grade points function")
+TEST_CASE("Test get grade points function")
 {
 	REQUIRE(get_grade_points("A") == 4);
+	REQUIRE(get_grade_points("B") == 3);
+	REQUIRE(get_grade_points("C") == 2);
+	REQUIRE(get_grade_points("D") == 1);
+	REQUIRE(get_grade_points("F") == 0);
+}
+
+TEST_CASE("Test calculate GPA")
+{
+	REQUIRE(calculate_gpa(12, 45)==3.75);
+	REQUIRE(calculate_gpa(120, 390) == 3.25);
+	REQUIRE(calculate_gpa(90, 180) == 2.00);
 }
