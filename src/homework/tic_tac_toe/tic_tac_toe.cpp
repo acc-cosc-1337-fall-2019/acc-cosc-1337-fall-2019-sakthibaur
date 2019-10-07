b3a8c4c225a63aad1f0df0d4869f9532eaef2743
Sakthi Baur <sakthi.baur@g.austincc.edu>
2019-10-06 20:13:13 -0500

    TicTacToe homework started

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,65 @@
+#include "tic_tac_toe.h"
 //cpp
+
+void TicTacToe::set_next_player()
+{
+	if(next_player=="X")
+	{
+		next_player = "O";
+	}
+	else
+	{
+		next_player = "X";
+	}
+}
+
+void TicTacToe::check_column_win()
+{
+
+}
+
+void TicTacToe::check_row_win()
+{
+
+}
+
+void TicTacToe::check_diagonal_win()
+{
+
+}
+
+void TicTacToe::clear_board()
+{
+}
+
+void TicTacToe::check_board_full()
+{
+
+}
+
+void TicTacToe::game_over()
+{
+
+}
+
+
+void TicTacToe::start_game(string player)
+{
+	next_player = player;
+	clear_board();
+}
+
+void TicTacToe : mark_board(int position)
+{
+
+}
+
+string TicTacToe::get_player() const;
+{
+	return next_player;
+}
+
+void TicTacToe::display_board() const
+{
+
+}
