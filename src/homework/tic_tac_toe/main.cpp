55b2f49129e7a7daab05256f459269d1325b945a
Sakthi Baur <sakthi.baur@g.austincc.edu>
2019-10-10 09:44:29 -0500

    tic tac toe homework

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,23 +1,25 @@
 #include "tic_tac_toe.h"
-#include<typeinfo>
 
 int main() 
 {
 	auto user_choice = 1;
 	int position;
 	string player;
-	//vector<int> valid_pos = { 1,2,3,4,5,6,7,8,9 };
+	vector<int> valid_pos = { 1,2,3,4,5,6,7,8,9 };
+	bool valid;
+
 	do
 	{
 		TicTacToe game;
 		cout << "Beginning player enter X or O: ";
 		cin >> player;
-		//check player selection, change to while loop?
+
 		while (player != "X" && player != "O")
 		{
 			cout << "Invalid player selection. Please enter X or O: ";
 			cin >> player;
 		}
+
 		//start game
 		game.start_game(player);
 
@@ -26,11 +28,13 @@ int main()
 			game.display_board();
 			cout << "Enter position for " << game.get_player() << ": ";
 			cin >> position;
-			while (position < 1 || position > 9)
+			//validates ints but not any other kind of input
+			while (position < 1 || position > 9) 
 			{
 				cout << "Invalid position. Please enter a number 1-9: ";
 				cin >> position;
 			}
+
 			game.mark_board(position);
 
 		} while (game.game_over() == false);
