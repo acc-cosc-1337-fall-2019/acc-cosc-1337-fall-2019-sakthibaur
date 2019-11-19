c0266878000b0ea7852d76cfcbe279553fcb1a87
sakthibaur <sakthi.baur@g.austincc.edu>
2019-11-18 18:18:50 -0600

    Example 11 Reference Pointers (01)

--- src/examples/11_module/01_ref_pointers/ref_pointers.cpp
+++ src/examples/11_module/01_ref_pointers/ref_pointers.cpp
@@ -1,9 +1,10 @@
 #include "ref_pointers.h"
 
-//
+//Show that references and pointers can modify data pointed to
 
 void ref(int &number, int* numberPtr)
 {
-	
+	number = 500;
+	*numberPtr = 1000;
 }
 
