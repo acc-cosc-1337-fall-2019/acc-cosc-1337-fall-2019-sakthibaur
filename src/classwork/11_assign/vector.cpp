4bec890287910b26ce8fcb31487e03bc52536293
sakthibaur <sakthi.baur@g.austincc.edu>
2019-11-20 19:09:45 -0600

    Classwork Assignment 11-Vector Class Copy, Copy Assignment, Move Constructor

--- src/classwork/11_assign/vector.cpp
+++ src/classwork/11_assign/vector.cpp
@@ -1,1 +1,61 @@
+#include "vector.h"
+#include<iostream>
 //
+
+Vector::Vector(size_t sz)
+	:size{sz}, nums{new int[sz]}
+{
+	for (size_t i = 0; i < sz; ++i)
+	{
+		nums[i] = 0;
+	}
+}
+
+Vector::Vector(const Vector & v)
+	:size{ v.size }, nums{ new int[v.size] }
+{
+	for (size_t i = 0; i < size; ++i)
+	{
+		nums[i] = v[i];
+	}
+}
+
+Vector & Vector::operator=(const Vector & v)
+{
+	int* temp = new int[v.size]; //creating temporary array (copy) of same size and values
+	
+	for (size_t i = 0; i < v.size; ++i)
+	{
+		temp[i] = v[i];
+	}
+
+	delete[] nums;
+	nums = temp;
+	size = v.size;
+	
+	return *this; //returning this instance of object at this instant
+}
+
+Vector::Vector(Vector && v)//move constructor
+	:size{v.size}, nums{v.nums}
+{
+	v.size = 0;
+	v.nums = nullptr;
+}
+
+Vector & Vector::operator=(Vector && v)//move assignment
+{
+	delete nums;
+	nums = v.nums;
+	size = v.size;
+	v.nums = nullptr;
+	v.size = 0;
+
+	return *this;
+}
+
+Vector::~Vector()
+{
+	std::cout << "delete array...";
+	delete[] nums;
+}
--- src/classwork/11_assign/vector.h
+++ src/classwork/11_assign/vector.h
@@ -1,1 +1,18 @@
 //
+
+class Vector
+{
+public:
+	Vector(size_t sz);
+	Vector(const Vector& v);//copy constructor
+	Vector& operator=(const Vector& v);//copy assignment
+	Vector(Vector&& v);//move constructor
+	Vector& operator=( Vector&& v);//move assignment
+	size_t Size()const { return size; }
+	int& operator[](int i) { return nums[i]; }
+	int& operator[](int i) const { return nums[i]; }
+	~Vector();//destructor
+private:
+	size_t size;
+	int* nums;
+};
